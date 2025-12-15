6dd79a740ce68899b836b53a9011de3408877f1b
Yuhaa-keren <naufalyuha29@gmail.com>
2025-12-16 01:18:30 +0700

    #2

--- database/migrations/2025_06_10_165548_create_warnings_table.php
+++ database/migrations/2025_06_10_165548_create_warnings_table.php
@@ -11,23 +11,15 @@ return new class extends Migration
         Schema::create('warnings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            // $table->text('description')->nullable(); 
-            // $table->string('level'); 
             $table->timestamps();
-            $table->foreignId('admin_id')->constrained('users')->onDelete('cascade'); // Admin yang mengeluarkan peringatan (asumsi tabel admin juga 'users')
+
+            // Kolom tambahan
+            $table->foreignId('admin_id')->constrained('users')->onDelete('cascade'); // Admin yang mengeluarkan peringatan
             $table->enum('warning_type', ['--Pilih Tipe--','Pelanggaran Aturan', 'Tindakan Akun (Blokir/Suspend)', 'Pengumuman Penting', 'Lain-lain'])->default('--Pilih Tipe--');
             $table->string('subject'); // Judul atau subjek peringatan
             $table->text('message'); // Isi pesan peringatan
             $table->enum('status', ['sent', 'read', 'resolved', 'pending_action'])->default('sent'); // Status peringatan
             $table->timestamp('expires_at')->nullable(); // Tanggal kadaluarsa (opsional)
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('admin_id')->constrained('users')->onDelete('cascade'); 
-            $table->string('warning_type'); 
-            $table->string('subject');
-            $table->text('message'); 
-            $table->enum('status', ['sent', 'read', 'resolved', 'pending_action'])->default('sent'); 
-            $table->timestamp('expires_at')->nullable(); 
-            $table->timestamps();
         });
     }
 
